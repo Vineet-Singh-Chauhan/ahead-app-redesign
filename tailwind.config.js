bc56b05df83bf379f066e61a603d2056c5bdc258
Vineet-Singh-Chauhan <chauhansvineet@gmail.com>
2024-03-29 10:31:33 +0530

    setup basic react app using vite along with tailwind,react-router-dom

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondaryFontColor: "#383838",
+      },
+    },
+  },
+  plugins: [],
+};
